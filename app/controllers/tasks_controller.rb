382e2dcb322bd45e561469063215c10826667ad4
nobuhiko okazaki <okazaki05040@gmail.com>
2017-04-25 01:27:59 +0000

    update add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,24 +5,22 @@ class TasksController < ApplicationController
   end
 
   def show
-      
   end
 
   def new
       @task = Task.new
-      
   end
 
   def create
       @task = Task.new(task_params)
 
-    if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new
-    end
+    　if @task.save
+      　flash[:success] = 'Task が正常に投稿されました'
+      　redirect_to @task
+    　else
+      　flash.now[:danger] = 'Task が投稿されませんでした'
+      　render :new
+    　end
   end
 
   def edit
@@ -51,11 +49,11 @@ class TasksController < ApplicationController
   
   private
   
-  
   def set_task
       @task = Task.find(params[:id])
   end
+  
   def task_params
-      params.require(:task).permit(:content)
+      params.require(:task).permit(:content, :status)
   end
 end
